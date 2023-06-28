972d1bcda9006b3a338ceb242f3e6fd9395c2634
MarioLDD <112513242+MarioLDD@users.noreply.github.com>
2023-06-28 17:00:36 -0300

    agrego modelos 3d

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
+    public Rigidbody playerRb;
     public List<AxleInfo> axleInfos; // the information about each individual axle
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
+    private float brakeInput;
+    public float maxBrakeTorque;
+    private Vector2 _movementInput;
+    public float fuerza;
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
 
+    private void Update()
+    {
+        Debug.Log(brakeInput);
+
+    }
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        float motor = maxMotorTorque * _movementInput.y;
+        float steering = maxSteeringAngle * _movementInput.x;
 
         foreach (AxleInfo axleInfo in axleInfos)
         {
@@ -26,7 +41,45 @@ public class CarController : MonoBehaviour
                 axleInfo.rightWheel.motorTorque = motor;
             }
         }
+        playerRb.AddRelativeForce(Vector3.down * fuerza, ForceMode.Force);
     }
+
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>(); 
+    }
+
+    private void OnBrake(InputValue inputValue)
+    {
+        brakeInput = inputValue.Get<float>();
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.brakeTorque = brakeInput * maxBrakeTorque;
+                axleInfo.rightWheel.brakeTorque = brakeInput * maxBrakeTorque;
+            }
+
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.brakeTorque = brakeInput * maxBrakeTorque;
+                axleInfo.rightWheel.brakeTorque = brakeInput * maxBrakeTorque;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 [System.Serializable]
@@ -34,6 +87,8 @@ public class AxleInfo
 {
     public WheelCollider leftWheel;
     public WheelCollider rightWheel;
+    public MeshRenderer meshRenderer_LW;
+    public MeshRenderer meshRenderer_RW;
     public bool motor; // is this wheel attached to motor?
     public bool steering; // does this wheel apply steer angle?
 }
